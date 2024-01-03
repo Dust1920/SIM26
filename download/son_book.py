37a8054df4077f76d5f72770612aae92a91e26db
Dust1920 <a218208885@gmail.com>
2024-01-03 11:47:26 -0700

    g

--- download/son_book.py
+++ download/son_book.py
@@ -2,14 +2,14 @@ import os
 import pandas as pd
 
 # Extraemos el listado de archivos tipo excel. 
-sonora_data = os.listdir('raw_data\\Municipios')
+sonora_data = os.listdir('..\\raw_data\\Municipios')
 
 # Definimos el constructor del archivo excel.
-excelw = pd.ExcelWriter('medium_data\\book_son.xlsx')
+excelw = pd.ExcelWriter('..\\medium_data\\book_son.xlsx')
 
 for mun in sonora_data:
     muns = mun[4:-5]
-    path = 'raw_data\\Municipios\\' + mun
+    path = '..\\raw_data\\Municipios\\' + mun
     raw = pd.read_excel(path)
     raw.to_excel(excelw, sheet_name=muns)
 
