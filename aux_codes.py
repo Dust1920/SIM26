004a56da27453944ae02fe60341d3245aafbf9cd
Dust1920 <a218208885@gmail.com>
2024-01-12 12:19:31 -0700

    reforma1

--- aux_codes.py
+++ aux_codes.py
@@ -1,5 +1,24 @@
-# Descargar archivos mediante internet
 
+# Información Descargada.
+raw_folder = "raw_data"
+
+# Información formateada para su conversión a tidy
+medium_folder = "medium_data"
+
+# Información estilizada en muchos archivos. 
+tidy_data = "tidy_data"
+
+# Información estilizada y almacenada en pocos archivos. 
+database_folder = 'databases'
+
+
+
+# (Funcion) Descargar archivos mediante internet
 import requests as r
-def descarga(url, save_path):
-    
+def descarga(url, save_path, filename):
+    html = r.get(url)
+    with open(save_path + '\\' + filename) as download:
+        download.write(r.content)
+
+
+print('Bien Hecho')
